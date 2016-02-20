c74081398949b2046c1bd87d3c58cacd9ef9a133
Francisco Souza <f@souza.cc>
2016-02-20 20:29:23 -0200

    install_docker: configure Docker to bind on tcp://0.0.0.0:2375

--- install_docker.sh
+++ install_docker.sh
@@ -5,5 +5,8 @@ echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc
 sudo apt-get update
 
 sudo apt-get install docker-engine=${1}-0~$(lsb_release -cs) -y --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+
+echo 'DOCKER_OPTS="$DOCKER_OPTS -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --insecure-registry=127.0.0.1:5000"' >> /etc/default/docker
+
 sudo stop docker || true
 sudo start docker
