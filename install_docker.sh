f7baf34ad0dcdd2be9d1735c12a2b78509e1f7f1
Francisco Souza <f@souza.cc>
2016-09-30 21:49:20 -0400

    install_docker: run dist-upgrade before installing docker

--- install_docker.sh
+++ install_docker.sh
@@ -4,6 +4,7 @@ release=$(lsb_release -cs)
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-${release} main" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-get update
+sudo apt-get dist-upgrade -y
 
 sudo apt-get install docker-engine=${1}-0~${release} -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
 
