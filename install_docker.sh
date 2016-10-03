4d09935e1884ccac85e5469b58527e2ae3774d57
Francisco Souza <f@souza.cc>
2016-10-03 14:59:10 -0400

    install_docker: script runs as root, no need for sudo
    And also, the whoami trick doesn't work :-(

--- install_docker.sh
+++ install_docker.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 release=$(lsb_release -cs)
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-${release} main" | sudo tee /etc/apt/sources.list.d/docker.list
-sudo apt-get update
-sudo apt-get dist-upgrade -y
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-${release} main" | tee /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get dist-upgrade -y
 
-sudo apt-get install docker-engine=${1}-0~${release} -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+apt-get install docker-engine=${1}-0~${release} -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
 
-sudo sed -i 's;^ExecStart=.*;ExecStart=/usr/bin/dockerd -H tcp://0.0.0.0:2375 -H fd:// --insecure-registry=127.0.0.1:5000 --insecure-registry=192.168.50.4:5000;' /lib/systemd/system/docker.service
+sed -i 's;^ExecStart=.*;ExecStart=/usr/bin/dockerd -H tcp://0.0.0.0:2375 -H fd:// --insecure-registry=127.0.0.1:5000 --insecure-registry=192.168.50.4:5000;' /lib/systemd/system/docker.service
 
-sudo systemctl daemon-reload
-sudo systemctl stop docker || true
-sudo systemctl start docker
-sudo usermod -G docker $(whoami)
+systemctl daemon-reload
+systemctl stop docker || true
+systemctl start docker
+usermod -G docker ubuntu
